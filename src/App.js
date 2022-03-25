9429007d5bd73f15ab6c8f7d6aaa109f14e15425
sumanmunde123 <93462136+sumanmunde123@users.noreply.github.com>
2022-03-25 11:37:44 +0530

    reactinnt

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <h1>
+       Mobile Operating System
+     </h1>
+     <ul>
+      <li> Android</li>
+      <li>Blackberry</li>
+      <li>iPhone</li>
+      <li>Windows Phone</li>
+      </ul>
+     
+     <br/>
+     <h1>
+       Mobile Manufactures
+     </h1>
+     <ul>
+       <li>Samsung</li>
+       <li>HTC</li>
+       <li>Micromax</li>
+       <li>Apple</li>
+       </ul>
+    
+    </div>
+  );
+}
+
+export default App;
